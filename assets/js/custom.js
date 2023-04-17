9260c77ef09b89e9673412a61ee04f770976723b
Bernard Amaitsa <besimani@gmail.com>
2023-04-18 00:02:53 +0300

    update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $("button").submit(function(){
-      $("p").show("slow", function(){
+      $("p").hide("slow", function(){
         alert("We have received your message. Thank you for reaching out to us");
         console.log(submit)
       });
