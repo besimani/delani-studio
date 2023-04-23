32de1fc7744eb3f059866b5f3c7f2eba84ccb93f
Bernard Amaitsa <besimani@gmail.com>
2023-04-23 22:13:16 +0300

    add toggle button

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
+  $("button").click(function(){
+    $("p").toggle(1000, function(){
+      alert("The toggle() method is finished!");
+    });
+  });
+});
+
+
+$(document).ready(function(){
     $("button").submit(function(){
       $("p").hide("slow", function(){
         alert("We have received your message. Thank you for reaching out to us");
